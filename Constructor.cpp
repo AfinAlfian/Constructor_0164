f9a9de3ba90f6534558dcf86639303eadf2ac9dc
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-14 10:12:12 +0700

    menambahkan member prosedur

--- Constructor.cpp
+++ Constructor.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 class mahasiswa {
 public:
     static int nim;
     int id;
     string nama;
+
+    void setID();
+    void printAll();
 };
 
 int main()
